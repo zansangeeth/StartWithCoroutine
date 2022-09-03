f915035205a044caf32f979e86d3656944d5a7ec
zansangeeth <zansangeeth17@gmail.com>
2022-09-03 11:34:57 +0530

    simple fake api call coroutine response

--- app/src/main/java/com/zasa/startwithcoroutine/MainActivity.kt
+++ app/src/main/java/com/zasa/startwithcoroutine/MainActivity.kt
@@ -1,27 +1,50 @@
 package com.zasa.startwithcoroutine
 
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.*
-import org.w3c.dom.Text
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "results 01"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnClickMe.setOnClickListener {
 
+            CoroutineScope(IO).launch {
+                fakeApiCall()
+                withContext(Main){
+                    tvText.text = fakeApiCall()
+                }
+            }
+
+
         }
+    }
 
+    private suspend fun fakeApiCall() : String{
+        val results1 = getResults1FromApi()
+        return results1
+    }
 
+    private suspend fun getResults1FromApi() : String{
+        logThread("getResults1FromApi")
+        delay(2000)
+        return RESULT_1
+    }
 
+    private fun logThread(methodName : String){
+        println("debug : $methodName - ${Thread.currentThread().name}")
     }
 
 
